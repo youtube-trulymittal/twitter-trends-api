f507855fc9559c3ac6c0016826b0290ef3b8e2f0
yuskara <ykaraca@gmail.com>
2021-11-04 14:49:53 -0400

    twitter api

--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -8,14 +8,43 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_SECRET,
 })
 
-// To get trending topics...
+// To get trending topics...  https://api.twitter.com/1.1/trends/place.json?id=1
 router.get('/trends', async (req, res, next) => {
   try {
-    const id = req.query.woeid
+    const id = req.query.woeid 
     const trends = await client.get('trends/place.json', {
       id,
     })
-    res.send(trends)
+    res.send(trends) 
+    // console.log(id)
+  } catch (error) {
+    console.log(error.message)
+    next(error)
+  }
+})
+// To get trending topics...  https://api.twitter.com/1.1/users/search.json?q=a&
+router.get('/mentions', async (req, res, next) => {
+  try {
+    const q = req.query.searchValue
+    const sample = await client.get('users/search.json?', {
+      q,
+    })
+    res.send(sample) 
+    // console.log(sample)
+  } catch (error) {
+    console.log(error.message)
+    next(error)
+  }
+})
+// To get trending hashtags...  https://api.twitter.com/1.1/search/tweets.json?q=from%3ACmdr_Hadfield%20%23nasa&result_type=popular
+router.get('/hashtags', async (req, res, next) => {
+  try {
+    const q = "%23"+req.query.searchHashtag
+    const sample = await client.get('search/tweets.json?q=&result_type=popular', {
+      q,
+    })
+    res.send(sample) 
+    console.log(sample)
   } catch (error) {
     console.log(error.message)
     next(error)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import './App.css'
 import { FaCrosshairs } from 'react-icons/fa'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Input,InputGroup,InputRightElement,Box } from "@chakra-ui/react"
 
 function App() {
   const [trends, setTrends] = useState([])
   const [woeid, setWoeid] = useState('1')
-
-  useEffect(() => getTrends(), [woeid])
+  const [users, setUsers] = useState([])
+  const [hashtags, setHashtags] = useState([])
+  const [searchValue, setSearchValue] = useState('life')
+  const [searchHashtag, setSearchHashtag] = useState('live')
+  useEffect(() => {getTrends(); getMensions(); getHashtags(); },[woeid])
 
   function getTrends() {
     axios
@@ -18,8 +22,36 @@ function App() {
         },
       })
       .then(response => {
-        // console.log(response.data)
+        console.log(response.data)
         setTrends(response.data[0].trends)
+      })
+      .catch(error => console.log(error.message))
+  }
+
+  function getMensions() {  
+    axios
+      .get('/api/mentions', { 
+        params: {
+          searchValue,
+        },
+       })
+      .then(response => {
+        console.log(response.data)
+        setUsers(response.data)
+      })
+      .catch(error => console.log(error.message))
+  }
+
+  function getHashtags() {  
+    axios
+      .get('/api/hashtags', { 
+        params: {
+          searchHashtag,
+        },
+       })
+      .then(response => {
+        console.log(response.data)
+        setHashtags(response.data.statuses)
       })
       .catch(error => console.log(error.message))
   }
@@ -66,6 +98,48 @@ function App() {
       </ul>
     )
   }
+
+  function listUsers() {
+    return (
+      <ul>
+        {users.map((user, index) => {
+          return (
+            <li key={index}>
+              <a href={user.url}>{user.name}</a>
+              {user.location && (
+                <span className='tweet_volume'>{user.location}</span>
+              )}
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+  function listHashtags() {
+    return (
+      <ul>
+        {hashtags.map((user, index) => {
+          return (
+            <li key={index}>
+              <a href={user.source}>{user.text}</a>
+              {/* {user.location && (
+                <span className='tweet_volume'>{user.location}</span>
+              )} */}
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  const handleChangeUsers= (e) => {
+    setSearchValue(e.target.value)
+    console.log(searchValue);
+  }
+  const handleChangeHashtags= (e) => {
+    setSearchHashtag(e.target.value)
+    console.log(searchHashtag);
+  }
   return (
     <div className='App'>
       <header className='App-header'>
@@ -79,13 +153,55 @@ function App() {
           <option value='2459115'>New York,US</option>
           <option value='2442047'>Los Angeles,US</option>
           <option value='2295411'>Mumbai</option>
-          <option value='1105779'>Sydney,AU</option>
+          <option value='1105779'>Sydney,AU</option> 
+          <option value='2343732'>Ankara,TR</option>
+          <option value='2344116'>Istanbul,TR</option>
         </select>
         <div className='location' onClick={handleLocation}>
           <FaCrosshairs />
         </div>
       </div>
       <div className='content'>{listTrends()}</div>
+      <header className='App-header'>
+        <img src={logo} className='logo' alt='twitter' />
+        <h3>Twitter Users</h3>
+      </header>
+      <div className='menu'>
+          <InputGroup size="md">
+              <Input
+                pr="4.5rem"
+                onChange={handleChangeUsers}
+                placeholder="Search Users "
+              />
+          <InputRightElement width="4.5rem">
+            <Box as="button" borderRadius="md" bg="tomato" color="white" 
+            value={searchValue} onClick={getMensions} alignItems="baseline" px={4} h={40}>
+                Search
+            </Box>
+          </InputRightElement>
+        </InputGroup>
+      </div>
+      <div className='content'>{listUsers()}</div>
+      
+      <header className='App-header'>
+        <img src={logo} className='logo' alt='twitter' />
+        <h3>Twitter Hashtags</h3>
+      </header>
+      <div className='menu'>
+          <InputGroup size="md">
+              <Input
+                pr="4.5rem"
+                onChange={handleChangeHashtags}
+                placeholder="Search Hashtags "
+              />
+          <InputRightElement width="4.5rem">
+              <Box as="button" borderRadius="md" bg="tomato" color="white" value={searchHashtag} onClick={getHashtags} px={4} h={40}>
+                 Search
+              </Box>
+          </InputRightElement>
+        </InputGroup>
+      </div>
+      <div className='content'>{listHashtags()}</div>
     </div>
   )
 }
